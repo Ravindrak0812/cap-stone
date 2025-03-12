287ec66fc725d22b720491ff96f27ec2c0deb5c9
Ravindra Kumar <ravindra8122003@gmail.com>
2025-03-12 16:24:07 +0530

    Saved local changes before pulling

--- server/model/User.js
+++ server/model/User.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },  // Name is required
+  name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  img: { type: String, default: '' }, // Store image URL
 });
 
 module.exports = mongoose.model('User', UserSchema);
